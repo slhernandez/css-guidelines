2f6a794aa1322dfdbf78102fd029821edb0c6ee0
Steve Hernandez <slhernandez@gmail.com>
2015-03-08 15:12:09 -0500

    Added nav control for workarea in demo.

--- build/scripts/bundle.js
+++ build/scripts/bundle.js
@@ -8,23 +8,31 @@ var demo = {
     var demoNav = document.querySelector('.demo-menu');
     var workarea = document.querySelector('.workarea');
     demoNav.addEventListener('click', function(e) {
-      console.log(e.target.textContent);
+      demo.clearNavSelection(demoNav);
+      e.target.classList.add('select');
       var menuItem = e.target.textContent;
       // clear out workarea
       if (menuItem === 'Layout Basics') {
-        console.log('Layout Basics is selected');
-        //workarea.querySelector('workarea-basics').classList.add('show');
+        demo.clearWorkArea(workarea);
+        workarea.querySelector('.workarea-basics').classList.add('show');
       } else if (menuItem === 'Scaling Button') {
-        console.log('Scaling Button is selected');
-        //workarea.querySelector('workarea-scaling').classList.add('show');
+        demo.clearWorkArea(workarea);
+        workarea.querySelector('.workarea-scaling').classList.add('show');
       } else if (menuItem === 'Magic Numbers') {
-        console.log('Magic Numbers is selected');
-        //workarea.querySelector('workarea-magicnumbers').classList.add('show');
+        demo.clearWorkArea(workarea);
+        workarea.querySelector('.workarea-magicnumbers').classList.add('show');
       }
     }.bind(demo));
   }, 
   clearWorkArea: function(workarea) {
-    console.log(workarea);
+    Array.prototype.map.call(workarea.children, function(item) {
+      item.classList.remove('show');
+    });
+  },
+  clearNavSelection: function(nav) {
+    Array.prototype.map.call(nav.children, function(item) {
+      item.classList.remove('select');
+    });
   }
 }
 
